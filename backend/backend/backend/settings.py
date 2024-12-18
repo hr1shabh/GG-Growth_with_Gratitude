2ca2e59fad973c54b481b189343755a5510ffd9e
Hrishabh <hrishabhpalsra@gmail.com>
2024-12-18 23:10:58 +0530

    configured Postgre

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ggDB',
+        'USER': 'gg',
+        'PASSWORD': 'gg',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
