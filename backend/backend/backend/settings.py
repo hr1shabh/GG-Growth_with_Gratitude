14eed8e5c57206b386082dc535e0c7cabfa11782
Hrishabh <hrishabhpalsra@gmail.com>
2024-12-20 23:29:00 +0530

    tried to make authentication/authorization and register APIs

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -131,3 +131,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'users.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
--- backend/backend/users/views.py
+++ backend/backend/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    print(request.data)  # Debugging: Print the incoming request data
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        print("Serializer is valid.")  # Debugging step
+        user = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
