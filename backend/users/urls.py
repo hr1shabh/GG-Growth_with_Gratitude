a0afbee2d29bf7c271785bf1f9cf2c7704459154
hr1shabh <hrishabhpalsra@gmail.com>
2026-02-15 11:01:05 +0530

    commit to check ci

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import register_user, get_user_profile, get_user_by_id
 
+#url patterns for users
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('profile/', get_user_profile, name='profile'),
     path('userprofile/<int:user_id>/', get_user_by_id, name='user-detail'),
 
 ]
+
+
